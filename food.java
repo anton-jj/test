abbd3813bf46eda1dcf96c9c9a10d1084f223955
pontus <pontus-forsberg@live.se>
2024-10-10 11:54:52 +0200

    tog bort onödigt text

--- food.java
+++ food.java
@@ -11,13 +11,11 @@ public class food {
         foodOptions.add("Köttbullar");
         foodOptions.add("Pizza");
         foodOptions.add("Pannkakor");
-<<<<<<< HEAD
 	foodOptions.add("kebab med bröd");
 	foodOptions.add("SmörgåsTårta");
-=======
         foodOptions.add("Korv"); //Olgas ändringar
         foodOptions.add("Glass"); //Olgas ändringar
->>>>>>> 6aa3d62b8e5cd60ce1036b5c4b6890786d24a723
+
 
     }
 }
